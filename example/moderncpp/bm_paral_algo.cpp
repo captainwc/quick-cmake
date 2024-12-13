f444e4cf8c2f106cc7cddb80a4b03c3ee2f3f4b2
shuaikai <wddjwk@qq.com>
2024-12-13 13:57:37 +0800

    [example moderncpp] add paral algo benchmark.

---
+++ example/moderncpp/bm_paral_algo.cpp
@@ -0,0 +1,30 @@
+#include <benchmark/benchmark.h>
+
+#include <execution>
+#include <numeric>
+#include <vector>
+
+#include "skutils/random.h"
+
+/**
+ * We can conlude that, when data size less than 70000, paral algorithm is no better than the trivial one.
+ */
+
+static std::vector<int> data{RANDPOOL.getRandomIntVector(70000)};
+
+static void bm_reduce(benchmark::State& state) {
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(std::reduce(data.begin(), data.end()));
+    }
+}
+
+static void bm_reduce_par(benchmark::State& state) {
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(std::reduce(std::execution::par, data.begin(), data.end()));
+    }
+}
+
+BENCHMARK(bm_reduce);
+BENCHMARK(bm_reduce_par);
+
+BENCHMARK_MAIN();
