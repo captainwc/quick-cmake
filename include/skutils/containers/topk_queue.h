85061aaaf9eb22b8949fa78cefe2a46db60b6e7e
shuaikai <wddjwk@qq.com>
2024-12-24 20:16:43 +0800

    [top_k_queue] refactor

--- include/skutils/containers/topk_queue.h
+++ include/skutils/containers/topk_queue.h
@@ -5,11 +5,13 @@
 #include <queue>
 #include <vector>
 
+#include "skutils/macro.h"
+
 namespace sk::utils::dts {
 
 template <typename T, typename Comp>
 struct reverse_comp {
-    struct type {
+    struct reversed_type {
         bool operator()(const T& a, const T& b) const {
             auto cmp = Comp();
             return (!cmp(a, b)) && (!(!cmp(a, b) && !cmp(b, a)));
@@ -20,13 +22,33 @@ struct reverse_comp {
 template <typename T, typename Comp = std::less<T>>
 class topk_queue {
 private:
-    size_t cap;
+    size_t cap{};
 
-    using ReversedComp = typename reverse_comp<T, Comp>::type;
+    using ReversedComp = typename reverse_comp<T, Comp>::reversed_type;
     std::priority_queue<T, std::vector<T>, ReversedComp> pq;
 
 public:
     explicit topk_queue(size_t capacity) : cap(capacity) {}
+
+    topk_queue(const topk_queue& q) : cap(q.cap), pq(q.pq) {}
+
+    topk_queue(topk_queue&& o) noexcept : cap(o.cap), pq(std::move(o.pq)) {}
+
+    topk_queue& operator=(const topk_queue& o) {
+        if (&o != this) {
+            this->cap = pq.cap;
+            this->pq  = o.pq;
+        }
+        return *this;
+    }
+
+    topk_queue& operator=(topk_queue&& o) noexcept {
+        this->cap = o.cap;
+        this->pq  = o.pq;
+        return *this;
+    }
+
+    ~topk_queue() = default;
 
     void push(const T& val) {
         if (pq.size() < cap) {
@@ -53,11 +75,32 @@ public:
 template <typename T, typename Comp = std::less<>>
 class topbottomk_queue {
 private:
-    topk_queue<T, Comp>                                 max_queue;
-    topk_queue<T, typename reverse_comp<T, Comp>::type> min_queue;
+    topk_queue<T, Comp>                                          max_queue;
+    topk_queue<T, typename reverse_comp<T, Comp>::reversed_type> min_queue;
 
 public:
     explicit topbottomk_queue(size_t capacity) : max_queue(capacity), min_queue(capacity) {}
+
+    topbottomk_queue(const topbottomk_queue& o) : max_queue(o.max_queue), min_queue(o.min_queue) {}
+
+    topbottomk_queue& operator=(const topbottomk_queue& o) {
+        if (&o != this) {
+            this->max_queue = o.max_queue;
+            this->min_queue = o.min_queue;
+        }
+        return *this;
+    }
+
+    topbottomk_queue(topbottomk_queue&& o) noexcept
+        : max_queue(std::move(o.max_queue)), min_queue(std::move(o.min_queue)) {}
+
+    topbottomk_queue& operator=(topbottomk_queue&& o) noexcept {
+        this->max_queue = o.max_queue;
+        this->min_queue = o.min_queue;
+        return *this;
+    }
+
+    ~topbottomk_queue() = default;
 
     void push(const T& val) {
         max_queue.push(val);
