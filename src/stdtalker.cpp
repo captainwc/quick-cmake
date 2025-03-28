4442573f1e2f845cb4f89e254a137e0a0e0d9ad8
shuaikai <wddjwk@qq.com>
2025-03-28 10:44:40 +0800

    [stdtalker] reduce the sep of threads sleeptime to get a quicker response.

--- src/stdtalker.cpp
+++ src/stdtalker.cpp
@@ -22,10 +22,21 @@ void Worker(double sep) {
     while (running.load()) {
         println("[{}] MSG {}", std::this_thread::get_id(), seq.fetch_add(1));
         ++cnt;
-        std::this_thread::sleep_for(std::chrono::duration<double>(sep));
+
+        // 将长时间睡眠分解为多次短时间睡眠，每次后检查running
+        auto const start_sleep    = std::chrono::steady_clock::now();
+        auto const total_sleep    = std::chrono::duration<double>(sep);
+        auto       remaining_time = total_sleep;
+
+        while (remaining_time > std::chrono::duration<double>::zero() && running.load()) {
+            // 每次最多睡眠500ms，减少延迟
+            auto const step = std::min(remaining_time, std::chrono::duration<double>(std::chrono::milliseconds(500)));
+            std::this_thread::sleep_for(step);
+            remaining_time = total_sleep - (std::chrono::steady_clock::now() - start_sleep);
+        }
     }
 
-    println("[ID-{}][{} => {}]: {}", std::this_thread::get_id(), start_time, time::current(), cnt);
+    // println("[ID-{}][{} => {}]: {}", std::this_thread::get_id(), start_time, time::current(), cnt);
 }
 
 int main(int argc, char** argv) {
@@ -37,14 +48,15 @@ int main(int argc, char** argv) {
         return -1;
     }
 
-    if (argc == 2) {
+    if (argc >= 2) {
         time_sep = std::atof(argv[1]);
     }
 
     if (argc == 3) {
-        time_sep   = std::atof(argv[1]);
-        thread_num = std::atof(argv[2]);
+        thread_num = std::atoi(argv[2]);  // 修正为std::atoi
     }
+
+    ThreadPool pool;
 
     auto sig_handler = [](int sig) {
         if (sig == SIGINT) {
@@ -54,15 +66,13 @@ int main(int argc, char** argv) {
 
     std::signal(SIGINT, sig_handler);
 
-    ThreadPool pool;
-
     std::vector<std::future<void>> futures;
     for (int i = 0; i < thread_num; i++) {
         futures.emplace_back(pool.submit(Worker, time_sep));
     }
 
     for (auto& f : futures) {
-        f.get();
+        f.get();  // 等待所有线程完成
     }
 
     return 0;
