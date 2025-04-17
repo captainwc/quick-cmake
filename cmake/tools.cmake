b57f0c92220313c84fd2fd9deaa3065e739483d1
shuaikai <wddjwk@qq.com>
2025-04-17 17:50:08 +0800

    [tool.cmake] 增强搜索库的方式，显示更多信息

--- cmake/tools.cmake
+++ cmake/tools.cmake
@@ -1,12 +1,12 @@
 # ANSI COLOR
-set(FG_RED    \\033[0;31m)
-set(FG_GREEN  \\033[0;32m)
+set(FG_RED \\033[0;31m)
+set(FG_GREEN \\033[0;32m)
 set(FG_YELLOW \\033[0;33m)
-set(FG_BLUE   \\033[0;36m)
-set(BG_RED    \\033[41m)
-set(BG_GREEN  \\033[42m)
+set(FG_BLUE \\033[0;36m)
+set(BG_RED \\033[41m)
+set(BG_GREEN \\033[42m)
 set(BG_YELLOW \\033[43m)
-set(BG_BLUE   \\033[44m)
+set(BG_BLUE \\033[44m)
 set(COLOR_RESET \\033[0m)
 # message red
 function(msg_red)
@@ -25,13 +25,42 @@ function(msg_blue)
     execute_process(COMMAND echo -en ${FG_BLUE}${ARGN}\n${COLOR_RESET})
 endfunction()
 
-# find package and specify message
+# find package and specify 
 macro(local_find_package)
+    # 第一个参数作为包名
     set(pkgname ${ARGV0})
+
     find_package(${ARGN} QUIET)
+
     if(${pkgname}_FOUND)
-        msg_green("[FOUNDED_PKG]: ${pkgname}" ${${pkgname}_INCLUDE_DIRS})
+        msg_green("[FOUNDED_PKG]: ${pkgname}")
+
+        # 尝试收集并显示包的所有重要路径信息
+        set(path_vars
+            ${pkgname}_INCLUDE_DIRS
+            ${pkgname}_INCLUDE_DIR
+            ${pkgname}_INCLUDES
+            ${pkgname}_LIBRARY
+            ${pkgname}_LIBRARIES
+            ${pkgname}_DIRS
+            ${pkgname}_DIR
+            ${pkgname}_ROOT
+            ${pkgname}_PATH
+            ${pkgname}_CONFIG)
+
+        # 循环检查每个可能的路径变量
+        foreach(path_var ${path_vars})
+            if(DEFINED ${path_var} AND NOT "${${path_var}}" STREQUAL "")
+                msg_blue("  - ${path_var}: ${${path_var}}")
+            endif()
+        endforeach()
+
+        # 显示版本信息(如果有)
+        if(DEFINED ${pkgname}_VERSION AND NOT "${${pkgname}_VERSION}" STREQUAL "")
+            msg_blue("  - Version: ${${pkgname}_VERSION}")
+        endif()
     else()
+        # 包未找到 - 以黄色显示警告信息
         msg_yellow("[UNFOUNDED_PKG]: ${pkgname}")
     endif()
 endmacro()
